d4071830777516e7e435e5ee4b08648d1ea0dcbf
pooja23182 <pooja23vijayakumar@gmail.com>
2026-03-30 10:19:17 +0530

    added files

---
+++ helloapp/src/main/java/org/example/UC2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class UC2 {
+    public static void main(String[] args){
+
+        // Read name from command line argument
+        String name = args[0];
+
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
+    }
+}
+
