b45469103272630895fd4e21ed4ccb8e07c38ebc
pooja23182 <pooja23vijayakumar@gmail.com>
2026-03-30 10:28:21 +0530

    added files

---
+++ helloapp/src/main/java/org/example/UC4.java
@@ -0,0 +1,19 @@
+package org.example;
+public class UC4 {
+    public static void main(String[] args) {
+
+        String result;
+
+        // Check if arguments are provided
+        if (args.length == 0) {
+            result = "World";
+        } else {
+            // Join all names with comma and space
+            result = String.join(", ", args);
+        }
+
+        // Display greeting
+        System.out.println("Hello, " + result + "!");
+    }
+}
+
